f9afa05ba4e76f5f29a74d2b66925fa05a48808b
itshMothy21 <timothyaudije@gmail.com>
2025-05-04 22:13:55 +0800

    jump

--- Songstress Project/Assets/C# Scripts/Player/PlayerMovement.cs
+++ Songstress Project/Assets/C# Scripts/Player/PlayerMovement.cs
@@ -3,54 +3,57 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 200f;
-    private Rigidbody2D rb;
+    private float speed = 6f;
+    private Rigidbody2D body;
     private SpriteRenderer sprite;
-    [SerializeField] private Animator animator;
-
-
+    private Animator anim;
+    private bool grounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
 
     //FixedUpdate is fixed within the device's framerate
-    private void FixedUpdate()
-    {
-        //Keyboard Inputs
-        // "Horizontal" is in Edit/Project Settings/Input Manager
-        float HorizontalInput = Input.GetAxisRaw("Horizontal");
-        float HorizontalMovement = HorizontalInput * speed * Time.deltaTime;
-        rb.linearVelocity = new Vector2 (HorizontalMovement, rb.linearVelocityY);
-
-        Anim(HorizontalInput);
-        if (Input.GetKey("a")) sprite.flipX = true;
-        else if (Input.GetKey("d")) sprite.flipX = false;
-        else if (Input.GetKey(KeyCode.LeftArrow)) sprite.flipX = true;
-        else if (Input.GetKey(KeyCode.RightArrow)) sprite.flipX = false;
-
-    }
-
     //Framerate of the game
     void Update()
     {
-        
+        //Keyboard Inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // move horizontically
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+
+        // left to right
+        if (horizontalInput > 0.01f)    
+        {
+            transform.localScale = new Vector3((float)1, (float)1, (float)1);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3((float)-1, (float)1, (float)1);
+        }
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+        // Set walk animation parameter
+        anim.SetBool("RunState", horizontalInput != 0);
+        anim.SetBool("Grounded", grounded);
     }
 
-    private void Anim(float moveInput)
+    private void Jump()
     {
-        if (moveInput != 0)
-        {
-            animator.SetBool("RunState", true);
-        }
-        else
-        {
-            animator.SetBool("RunState", false);
-        }
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
